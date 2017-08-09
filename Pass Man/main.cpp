00d35713d1aa56d8de343458df4a2ea94246cb7f
Kerndog73 <kerndog73@gmail.com>
2017-08-09 17:50:14 +0930

    Initial Commit

---
+++ Pass Man/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pass Man
+//
+//  Created by Indi Kernick on 9/8/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
